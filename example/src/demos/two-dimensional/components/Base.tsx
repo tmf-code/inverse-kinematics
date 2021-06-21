a01fbcaccbdc9bf48e1751465e8fbbb1f9771e98
Jae Perris <jae.perris@gmail.com>
2021-06-21 13:07:01 +0200

    Use variable learning rate for 2D example

--- example/src/demos/two-dimensional/components/Base.tsx
+++ example/src/demos/two-dimensional/components/Base.tsx
@@ -1,5 +1,5 @@
 import { useFrame } from '@react-three/fiber'
-import { Solve2D, V2 } from 'ik'
+import { MathUtils, Solve2D, V2 } from 'ik'
 import React, { useMemo, useRef } from 'react'
 import { BoxBufferGeometry, Mesh, MeshNormalMaterial } from 'three'
 import { Link, LinkProps } from './Link'
@@ -12,7 +12,7 @@ export const Base = ({ position, links, target }: { links: Solve2D.Link[]; posit
   useFrame(() => {
     if (!ref.current) return
     ref.current.position.set(...position, 0)
-    adjustedLinksRef.current = Solve2D.solve(links, position, target).links
+    adjustedLinksRef.current = Solve2D.solve(links, position, target, { learningRate, acceptedError: 10 }).links
 
     let depth = 0
     let child = chain
@@ -48,4 +48,20 @@ function makeChain(links: Solve2D.Link[]): LinkProps | undefined {
   }
 
   return chain
+}
+
+const knownRangeOfMovement = 230
+function learningRate(errorDistance: number): number {
+  const relativeDistanceToTarget = MathUtils.clamp(errorDistance / knownRangeOfMovement, 0, 1)
+  const cutoff = 0.1
+
+  if (relativeDistanceToTarget > cutoff) {
+    return 10e-5
+  }
+
+  // result is between 0 and 1
+  const remainingDistance = relativeDistanceToTarget / 0.02
+  const minimumLearningRate = 10e-6
+
+  return minimumLearningRate + remainingDistance * 10e-6
 }
