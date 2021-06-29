b49a06db16966e50ade37a77d0d1ff63de790a3c
Jae Perris <jae.perris@gmail.com>
2021-06-29 18:47:13 +0200

    Make jest watch ignore useless folders

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', './example/', './dist/'],
+  watchPathIgnorePatterns: ['/node_modules/', './example/', './dist/'],
 }
