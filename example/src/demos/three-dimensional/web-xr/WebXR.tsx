17373e9b6787a1d60ed4606599bc9f160c84144b
Alexandra <barancova.alexandra@gmail.com>
2021-07-13 18:35:25 +0200

    Add model scaling to webxr example

--- example/src/demos/three-dimensional/web-xr/WebXR.tsx
+++ example/src/demos/three-dimensional/web-xr/WebXR.tsx
@@ -2,10 +2,12 @@ import { useGLTF } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import { DefaultXRControllers, useController, VRCanvas } from '@react-three/xr'
 import { MathUtils, QuaternionO, Solve3D, V3O } from 'inverse-kinematics'
-import React, { Suspense, useRef } from 'react'
+import React, { Suspense, useRef, useState } from 'react'
 import * as THREE from 'three'
 import { Bone, MeshNormalMaterial, Vector3 } from 'three'
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Base } from '../components/Base'
+import { JointTransforms } from '../components/JointTransforms'
 import modelSrc from './arm2.gltf?url'
 
 type GLTFResult = GLTF & {
@@ -35,6 +37,9 @@ function Scene() {
   const { nodes } = useGLTF(modelSrc) as GLTFResult
   const modelRef = useRef<THREE.Group>()
 
+  const [base] = useState<Solve3D.JointTransform>({ position: V3O.zero(), rotation: QuaternionO.zeroRotation() })
+  const [debugLinks, setLinks] = useState<Solve3D.Link[]>([])
+
   const controller = useController('left')
 
   useFrame(() => {
@@ -48,6 +53,10 @@ function Scene() {
       rotation: QuaternionO.zeroRotation(),
     }
 
+    base.position = baseTransform.position
+    base.rotation = baseTransform.rotation
+
+    const modelScale = V3O.fromVector3(modelRef.current!.getWorldScale(new Vector3()))
     const bones: Bone[] = getBones(firstBone)
 
     const links: Solve3D.Link[] = bones.map((bone, index, array) => {
@@ -55,31 +64,18 @@ function Scene() {
       const rotation = QuaternionO.fromObject(bone.quaternion)
 
       return {
-        position: V3O.fromVector3(nextBone?.position ?? bone.position),
+        position: V3O.multiply(V3O.fromVector3(nextBone?.position ?? bone.position), modelScale),
         rotation: rotation,
       }
     })
 
     const knownRangeOfMovement = links.reduce((acc, cur) => acc + V3O.euclideanLength(cur.position), 0)
-    function learningRate(errorDistance: number): number {
-      const relativeDistanceToTarget = MathUtils.clamp(errorDistance / knownRangeOfMovement, 0, 1)
-      const cutoff = 0.1
-
-      if (relativeDistanceToTarget > cutoff) {
-        return 10e-4
-      }
-
-      const remainingDistance = relativeDistanceToTarget / 0.02
-      const minimumLearningRate = 10e-5
-
-      return minimumLearningRate + remainingDistance * 10e-5
-    }
 
     if (controller) {
       const target = V3O.fromVector3(controller?.controller.getWorldPosition(new Vector3()))
 
       const results = Solve3D.solve(links, baseTransform, target, {
-        learningRate,
+        learningRate: learningRate(knownRangeOfMovement),
         acceptedError: knownRangeOfMovement / 1000,
       }).links
 
@@ -87,6 +83,8 @@ function Scene() {
         const bone = bones[index]!
         bone.quaternion.set(...link.rotation)
       })
+
+      setLinks(results)
     }
   })
 
@@ -94,15 +92,17 @@ function Scene() {
     <>
       <ambientLight />
       <DefaultXRControllers />
+      <Base base={base} links={debugLinks} />
+      <JointTransforms base={base} links={debugLinks} />
       <Suspense fallback={null}>
-        <group dispose={null} position={[-10, 0, -10]}>
+        <group dispose={null} scale={[0.2, 0.2, 0.2]} position={[-1, 1.5, -2]}>
           <group ref={modelRef} dispose={null}>
             <primitive object={nodes.shoulder} />
             <skinnedMesh
               geometry={nodes.Cylinder.geometry}
               material={new MeshNormalMaterial({ wireframe: true })}
               skeleton={nodes.Cylinder.skeleton}
-            ></skinnedMesh>
+            />
           </group>
         </group>
         <skeletonHelper args={[nodes.shoulder]} />
@@ -124,3 +124,19 @@ function getBones(firstBone: THREE.Bone) {
 }
 
 useGLTF.preload(modelSrc)
+
+function learningRate(totalLength: number): (errorDistance: number) => number {
+  return (errorDistance: number) => {
+    const relativeDistanceToTarget = MathUtils.clamp(errorDistance / totalLength, 0, 1)
+    const cutoff = 0.1
+
+    if (relativeDistanceToTarget > cutoff) {
+      return 10e-4
+    }
+
+    const remainingDistance = relativeDistanceToTarget / 0.02
+    const minimumLearningRate = 10e-5
+
+    return minimumLearningRate + remainingDistance * 10e-5
+  }
+}
